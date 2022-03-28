fb2f8afa0b13b83802075298968b3e919a3e79b6
alexandru-ghibea <98416718+alexandru-ghibea@users.noreply.github.com>
2022-03-28 15:40:26 +0300

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,11 +1,15 @@
+'''
+Send text emails
+'''
+
 import smtplib
 from email.message import EmailMessage
 import getpass
 smtp_object = smtplib.SMTP("smtp.gmail.com", 587)
 smtp_object.ehlo()
 smtp_object.starttls()
-email = getpass.getpass("Email: ")
-password = getpass.getpass("Password: ")
+email = getpass.getpass("Input Email: ")
+password = getpass.getpass("Input Password: ")
 smtp_object.login(email, password)
 from_address = email
 to_address = email
